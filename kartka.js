2478f713b17beae730c15db310310dc0e1b789df
agatulemtu <121102575+agatulemtu@users.noreply.github.com>
2022-12-20 13:38:30 +0100

    Add files via upload

--- kartka.js
+++ kartka.js
@@ -1,10 +1,28 @@
 import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
 
 // initialize kaboom context
-kaboom();
+kaboom({
+    "fullscreen":"true",
+    "debug":"true"
+});
 
 // add a piece of text at position (120, 80)
+// add([
+//     text("hello"),
+//     pos(120, 80),
+// ]);
+
+loadSprite("tlo", "tlo.png")
+
+loadSprite("stefan", "stefan.png")
+
 add([
-    text("hello"),
-    pos(120, 80),
-]);
+    sprite("tlo"),
+    pos(0,0)
+])
+
+add([
+    sprite("stefan"),
+    pos(100,100),
+    area(),
+])
